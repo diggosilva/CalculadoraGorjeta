6b6124458fb003ff3c467d32ceb462b0e77088cc
Diggo Silva <rdiggosilva@gmail.com>
2026-04-25 16:08:48 -0300

    refactor: xcode configuration

---
+++ CalculadoraGorjetaUITests/CalculadoraGorjetaUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  CalculadoraGorjetaUITestsLaunchTests.swift
+//  CalculadoraGorjetaUITests
+//
+//  Created by Diggo Silva on 24/04/26.
+//
+
+import XCTest
+
+final class CalculadoraGorjetaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
