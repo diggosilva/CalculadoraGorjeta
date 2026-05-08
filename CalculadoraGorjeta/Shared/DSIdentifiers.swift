cdcd9b812ffd8770ea0323cc926fa207a495bfbe
Diggo Silva <rdiggosilva@gmail.com>
2026-05-08 14:15:36 -0300

    feat: create DSIdentifiers enum for centralized accessibility keys

--- CalculadoraGorjeta/Shared/DSIdentifiers.swift
+++ CalculadoraGorjeta/Shared/DSIdentifiers.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+enum DSIdentifiers {
+    static let clearButton = "clearButton"
+    
+    static let amountTextField = "amountTextField"
+    
+    static let tenPercentButton = "tenPercentButton"
+    static let fifteenPercentButton = "fifteenPercentButton"
+    static let twentyPercentButton = "twentyPercentButton"
+    static let twentyFivePercentButton = "twentyFivePercentButton"
+    static let customTipButton = "customTipButton"
+    
+    static let minusButton = "minusButton"
+    static let totalPeopleValue = "totalPeopleValue"
+    static let plusButton = "plusButton"
+    
+    static let totalTipValue = "totalTipValue"
+    static let totalPerPersonValue = "totalPerPersonValue"
+    static let grandTotalValue = "grandTotalValue"
+    static let shareButton = "shareButton"
+    
+    static let customTipAlert = "customTipAlert"
+    static let customTipTextField = "customTipTextField"
+}
